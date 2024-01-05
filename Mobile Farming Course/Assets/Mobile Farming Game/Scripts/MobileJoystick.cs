dd5d480bbb2c443685c61c1b7ca5b33bed9ad68f
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-05 09:03:03 +0200

    5 Coding time

--- Mobile Farming Course/Assets/Mobile Farming Game/Scripts/MobileJoystick.cs
+++ Mobile Farming Course/Assets/Mobile Farming Game/Scripts/MobileJoystick.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class MobileJoystick : MonoBehaviour
 {
-   
+   [Header("Elements")]
+   [SerializeField] private RectTransform joyStickOutline;
+   Vector3 clickedPosition;
+
+   public void ClickedOnJoyStickZoneCallback(){
+    clickedPosition = Input.mousePosition;
+    joyStickOutline.position = clickedPosition;
+   }
 }
