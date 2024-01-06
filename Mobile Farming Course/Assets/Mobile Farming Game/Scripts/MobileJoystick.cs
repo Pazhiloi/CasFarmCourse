bf5e8e09302254d8dc4a42bbc7932a0e8e7f1519
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-06 08:05:14 +0200

    6 moving the knob

--- Mobile Farming Course/Assets/Mobile Farming Game/Scripts/MobileJoystick.cs
+++ Mobile Farming Course/Assets/Mobile Farming Game/Scripts/MobileJoystick.cs
@@ -4,12 +4,52 @@ using UnityEngine;
 
 public class MobileJoystick : MonoBehaviour
 {
-   [Header("Elements")]
-   [SerializeField] private RectTransform joyStickOutline;
-   Vector3 clickedPosition;
+  [Header("Elements")]
+  [SerializeField] private RectTransform joyStickOutline;
+  [SerializeField] private RectTransform joyStickKnob;
 
-   public void ClickedOnJoyStickZoneCallback(){
+  [Header("Settings")]
+  private bool canControl;
+  Vector3 clickedPosition;
+  Vector3 currentPosition;
+  Vector3 direction;
+  private void Start()
+  {
+    HideJoystick();
+  }
+
+  private void Update()
+  {
+    if (canControl)
+    {
+      ControlJoystick();
+    }
+  }
+
+  public void ClickedOnJoyStickZoneCallback()
+  {
     clickedPosition = Input.mousePosition;
     joyStickOutline.position = clickedPosition;
-   }
+
+    ShowJoystick();
+  }
+
+  private void ShowJoystick()
+  {
+    joyStickOutline.gameObject.SetActive(true);
+    canControl = true;
+  }
+  private void HideJoystick()
+  {
+    joyStickOutline.gameObject.SetActive(false);
+    canControl = false;
+  }
+
+  private void ControlJoystick()
+  {
+    currentPosition = Input.mousePosition;
+    direction = currentPosition - clickedPosition;
+    joyStickKnob.position = joyStickOutline.position + direction;
+  }
+
 }
