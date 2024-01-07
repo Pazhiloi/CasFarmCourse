fa9d64ac7fdc9b41814d3dcb6daa004f88422cb4
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-07 07:47:13 +0200

    14 using animation Events

---
+++ Mobile Farming Course/Assets/Mobile Farming Game/Scripts/Player/PlayerAnimationEvents.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+   [Header("Elements")]
+   [SerializeField] private ParticleSystem seedParticles;
+
+   private void PlaySeedParticles(){
+    seedParticles.Play();
+   }
+}
