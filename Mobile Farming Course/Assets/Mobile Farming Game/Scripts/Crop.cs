1fa051c52928e8988ceb0796bf055f8d18033af7
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-16 09:55:45 +0200

    48 Polishing the Harvesting Process

--- Mobile Farming Course/Assets/Mobile Farming Game/Scripts/Crop.cs
+++ Mobile Farming Course/Assets/Mobile Farming Game/Scripts/Crop.cs
@@ -6,10 +6,15 @@ public class Crop : MonoBehaviour
 {
    [Header("Elements")]
    [SerializeField] private Transform cropRenderer;
+   [SerializeField] private ParticleSystem harvestedParticles;
    public void ScaleUp(){
     cropRenderer.gameObject.LeanScale(Vector3.one, 1f).setEase(LeanTweenType.easeOutBack);
    }
    public void ScaleDown(){
     cropRenderer.gameObject.LeanScale(Vector3.zero, 1f).setEase(LeanTweenType.easeOutBack).setOnComplete(() => Destroy(gameObject));
+
+    harvestedParticles.transform.parent = null;
+    harvestedParticles.gameObject.SetActive(true);
+    harvestedParticles.Play();
    }
 }
