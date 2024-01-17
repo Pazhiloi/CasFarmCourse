bad0687aa6df86d207fc8e1320cdeef8cdd87f8b
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-17 07:56:54 +0200

    52 Saving loading our inventory

--- Mobile Farming Course/Assets/Mobile Farming Game/Scripts/Inventory/InventoryManager.cs
+++ Mobile Farming Course/Assets/Mobile Farming Game/Scripts/Inventory/InventoryManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
    private Inventory inventory;
+   private string dataPath;
 
    private void Start() {
-    inventory = new Inventory();
-
+    // inventory = new Inventory();
+    dataPath = Application.dataPath + "/inventoryData.txt";
+    LoadInventory();
     CropTile.onCropHarvested += CropHarvestedCallback;
    }
 
@@ -18,5 +21,29 @@ public class InventoryManager : MonoBehaviour
 
   private void CropHarvestedCallback (CropType cropType) {
     inventory.CropHarvestedCallback(cropType);
+    SaveInventory();
+  }
+
+  private void LoadInventory() {
+    string data = "";
+    if (File.Exists(dataPath))
+    {
+      data = File.ReadAllText(dataPath);
+      inventory = JsonUtility.FromJson<Inventory>(data);
+      if (inventory == null)
+      {
+        inventory = new Inventory();
+      }
+    }else
+    {
+      File.Create(dataPath);
+      inventory = new Inventory();
+    }
+
+
+  }
+  private void SaveInventory() {
+    string data = JsonUtility.ToJson(inventory, true);
+    File.WriteAllText(dataPath, data);
   }
 }
